e18c8d734952eb810a6e0e65583a90ca061a4862
Amelia Miner <amminer@pdx.edu>
2022-05-24 21:10:13 -0700

    pytest is now broken :(

--- tests/test_grade.py
+++ tests/test_grade.py
@@ -1,18 +1,32 @@
 import __init__
 from Grade import Grade
+from random import randint
 g = Grade()
 
-def test_pointsPossible(): #TODO do tests work like this ? ...
-    for i in range(-100, 100):
+def test_points() -> None: #TODO do tests work like this ? ...
+    for i in range(100000):
+            i = randint(-100,1000000)
+            j = randint(-100,1000000)
+            g.pointsPossible = i
+            g.pointsEarned = j
+            assert g.pointsPossible >= 0
+            assert g.pointsEarned >= 0
+            assert g.pointsEarned <= g.pointsPossible
+
+
+def test_pointsPossible() -> None:
+    for i in range(-1000, 1000):
         g.pointsPossible = i
-        assert g._pointsPossible >= 0
-        assert g._pointsEarned <= g._pointsPossible
+        assert g.pointsPossible >= 0
+        assert g.pointsEarned <= g.pointsPossible
 
-def test_pointsEarned(): #TODO or like this?
-    assert g._pointsEarned >= 0
-    assert g._pointsEarned <= g._pointsPossible
+def test_pointsEarned() -> None:
+    for i in range(-1000, 1000):
+        g.pointsEarned = i
+        assert g.pointsEarned >= 0
+        assert g.pointsEarned <= g.pointsPossible
 
-def test_passes():
+def test_passes() -> None:
     gs = [Grade("midterm one", pointsPossible=20, pointsEarned=15),
           Grade("midterm two", pointsPossible=20, pointsEarned=9),
           Grade("some quiz", 10, 6)]
