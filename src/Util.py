296635c3caf498afb1a6e1f1df2e5bf92f12de1c
Amelia Miner <ameliamminer@gmail.com>
2022-08-14 16:38:01 -0700

    Found local files - not yet on git

--- src/Util.py
+++ src/Util.py
@@ -7,10 +7,11 @@ getStr, getPosInt, printBadInput,
 and presentInterface for generalized menu-ing
 """
 
-class Util():
+
+class Util:
     cursor = "~>"
 
-    def __init__(self, newName:str="NOT SET"):
+    def __init__(self, newName: str = "NOT SET"):
         self.name = newName
 
     @property
@@ -18,75 +19,80 @@ class Util():
         return self._name
 
     @name.setter
-    def name(self, newName:str):
+    def name(self, newName: str):
         if len(newName) > 0:
             self._name = str(newName)
 
-    #handles any RecursionError or ValueError
-    #(must be called from leaf-class's edit() function!)
+    # handles any RecursionError or ValueError
+    # (must be called from leaf-class's edit() function!)
     def editName(self):
-        print("Enter a new name" 
+        print(
+            "Enter a new name"
             + "(recommended maximum of 39 characters for display integrity)",
-              sep = '\n')
+            sep="\n",
+        )
         try:
             newName = self.getStr(1)
             self.name = newName
         except ValueError as ve:
             print(ve)
             self.setName()
-        except RecursionError as re: #user cancels or recursion depth exceeded
-            print(re) #return to calling scope
+        except RecursionError as re:  # user cancels or recursion depth exceeded
+            print(re)  # return to calling scope
 
-    #may throw RecursionError or ValueError
-    #(float('inf') produces a number larger than all others)
-    def getPosInt(self, min:int=0, max:int=float('inf')) -> int:
-        ret = input(self.cursor) #cast to int?
+    # may throw RecursionError or ValueError
+    # (float('inf') produces a number larger than all others)
+    def getPosInt(self, min: int = 0, max: int = float("inf")) -> int:
+        ret = input(self.cursor)  # cast to int?
+        print()
         if ret == "!q":
             raise RecursionError("Canceled!")
         else:
             ret = int(ret)
         if ret < min or ret > max:
             raise ValueError(f"Input must be between {min} and {max} inclusive\n")
-        return ret #TODO test
-    
-    def getStr(self, min:int=0) -> str:
-        ret:str = str(input(self.cursor))
+        return ret  # TODO test
+
+    def getStr(self, min: int = 0) -> str:
+        ret: str = str(input(self.cursor))
+        print()
         if ret == "!q":
             raise RecursionError("Canceled!")
         if len(ret) < min:
             raise ValueError(f"Input must be at least {min} characters\n")
-        return ret #TODO test
+        return ret  # TODO test
 
-    def printBadInput(self, badInput:str="") -> None:
+    def printBadInput(self, badInput: str = "") -> None:
         output = ""
         output += "Invalid input!"
         if badInput != None and badInput != "":
-            output += f" (\"{badInput}\" is bad input)"
+            output += f' ("{badInput}" is bad input)'
         print(output)
         return
 
-        """ PRESENTINTERFACE
-                Takes a string prompt, a list of strings `options`, and
-            a list of functions 'routines' which must be set up as parallel
-            arrays.
-                Prints the prompt and the options, takes input, checks input
-            against the options and calls *ALL* matching-indexed function calls
-            based on the strings in the options list.
-                subroutines must take no args (for now? TODO)
-        """ 
-    #May raise a RE that must be handled in client code!
-    #Handles own ValueErrs
-    def presentInterface(self, prompt:str, options:List[str],
-                         routines:List[Callable]):
+    """ PRESENTINTERFACE
+            Takes a string prompt, a list of strings `options`, and
+        a list of functions 'routines' which must be set up as parallel
+        arrays.
+            Prints the prompt and the options, takes input, checks input
+        against the options and calls *ALL* matching-indexed function calls
+        based on the strings in the options list.
+            subroutines must take no args (for now? TODO)
+    """
+    # May raise a RE that must be handled in client code!
+    # Handles own ValueErrs
+    def presentInterface(
+        self, prompt: str, options: List[str], routines: List[Callable]
+    ):
         print(prompt)
         try:
-            choice = self.getStr() #! May raise RecursionError if user cancels
-            for opt in options:    #  Catch in client code
+            choice = self.getStr()  #! May raise RecursionError if user cancels
+            for opt in options:  #  Catch in client code
                 if opt == choice:
-                    routines[options.index(opt)]()  #call subroutine
+                    routines[options.index(opt)]()  # call subroutine
                     break
-            else: #only executes if break is not reached
+            else:  # only executes if break is not reached
                 raise ValueError(f"{choice} is not a valid option\n")
         except ValueError as ve:
-                print(ve)
-                self.presentInterface(prompt, options, routines)
+            print(ve)
+            self.presentInterface(prompt, options, routines)
